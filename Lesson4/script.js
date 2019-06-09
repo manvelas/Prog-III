83ee0304ff7cfdc084ea019ecd6bcc975347b2ad
manvelas <manvel.asatryan.y@tumo.org>
2019-06-09 16:06:39 +0400

    sdssadwqfa

---
+++ Lesson4/script.js
@@ -0,0 +1,28 @@
+function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+    var del =  document.getElementById('delete');
+ 
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    function handleDelete(){
+        socket.emit("delete message");
+    }
+    button.onclick = handleSubmit;
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+}
+
+socket.on('display message', handleMessage);
+} // main closing bracket
+
+window.onload = main;   
